5c94453a45f90015ff9605d770d364cb7edc3642
Wilder Rincon <1588@holbertonschool.com>
2020-06-30 11:42:16 -0500

    Adding file .gitignore and file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -20,4 +20,24 @@ class FileStorage:
     if obj:
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
 
-    
+
+    def save(self):
+        """serializes __objects to __file_path"""
+        obj_dict = {}
+        for object_id, ob in self.__objects.items():
+            obj_dict[object_id] = ob.to_dict()
+        with open(self.__file_path, 'w', encoding="UTF-8") as f:
+            json.dump(obj_dict, f)
+
+
+    def reload(self):
+        """deserializes __objects from __file_path"""
+        try:
+            with open(self.__file_path, encoding="UTF-8") as f:
+                obj = json.load(f)
+            for obj_id, dictionary in obj.items():
+                val = models.cls_dict[dictionary['__class__']](**dictionary)
+                self.__objects[obj_id] = val
+        except FileNotFoundError:
+            pass
+
