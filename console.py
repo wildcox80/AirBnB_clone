c4ba65a304d75bd890ea0c2e5efac790f7a4ccfe
Wilder Rincon <1588@holbertonschool.com>
2020-06-30 17:14:46 -0500

    Adding changes file console.py

--- console.py
+++ console.py
@@ -9,12 +9,10 @@ class HBNBCommand(cmd.Cmd):
 
     def do_quit(self, arg):
         """Quit command to exit the console\n"""
-        quit()
         return True
 
     def do_EOF(self, arg):
         """EOF command to exit the console\n"""
-        quit()
         return True
 
     def do_help(self, arg):
