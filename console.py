87d67f4ae2f2e0f547d17c02cb71a841719821ae
Wilder Rincon <1588@holbertonschool.com>
2020-06-30 15:55:44 -0500

    Adding changes file console.py

--- console.py
+++ console.py
@@ -3,6 +3,26 @@
 import cmd
 
 
-    class HBNBCommand(cmd.Cmd):
-        """Console AIRBNB"""
-        prompt = '(hbnb) '
+class HBNBCommand(cmd.Cmd):
+    """Console AIRBNB"""
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit command to exit the console\n"""
+        quit()
+        return True
+
+    def do_EOF(self, arg):
+        """EOF command to exit the console\n"""
+        quit()
+        return True
+
+    def do_help(self, arg):
+        """Help command to get info on specified command
+        If no parameter given, list all commands
+        """
+        cmd.Cmd.do_help(self, arg)
+
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
