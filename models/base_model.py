2a4108626a02b897a9c93ef6340ab7a5df5b3205
Wilder Rincon <1588@holbertonschool.com>
2020-06-30 22:12:42 -0500

    Adding changes file base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -23,15 +23,17 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """ Return the human readable print format"""
         a, b, c = self.__class__.__name__, self.id, self.__dict__
-        return("[{}] ({}) {}".format(a, b, c))
+        return "[{}] ({}) {}".format(a, b, c)
 
     def save(self):
         """Shows the newly updated time from time of instance creation"""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """Returns a dictionary containing all keys/values"""
