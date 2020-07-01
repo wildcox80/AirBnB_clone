17e31d5627dd93f8c53439922ef384c97a44c309
Wilder Rincon <1588@holbertonschool.com>
2020-06-30 19:17:58 -0500

    Adding changes file base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,46 +1,48 @@
 #!/usr/bin/python3
 """ Modules for Class """
 
-import datetime
-import uuid
 from datetime import datetime
-from uuid import uuid4
+import json
+import uuid
+import models
 
 
 class BaseModel:
-    """Class Base Model"""
+    """Base Class with public instance attributes and methods"""
 
     def __init__(self, *args, **kwargs):
-        """ Constructor Method """
-
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-
+        """Instantiates the attributes of the BaseModel class"""
+        if args is not None and len(args) > 0:
+            pass
+        if kwargs:
+            for key, item in kwargs.items():
+                if key in ['created_at', 'updated_at']:
+                    item = datetime.strptime(item, "%Y-%m-%dT%H:%M:%S.%f")
+                if key not in ['__class__']:
+                    setattr(self, key, item)
         else:
-            for attr_name, attr in kwargs.items():
-                if attr_name == "created_at" or attr_name == "updated_at":
-                    attr = datetime.strptime(attr, "%Y-%m-%dT%H:%M:%S.%f")
-                if attr_name != "__class__":
-                    setattr(self, attr_name, attr)
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
-        """ String Method """
-
-        return("[{}] ({}) {}"
-               .format(type(self).__class__, self.id, self.__dict__))
+        """ Return the human readable print format"""
+        a, b, c = self.__class__.__name__, self.id, self.__dict__
+        return("[{}] ({}) {}".format(a, b, c))
 
     def save(self):
-        """Save instance"""
-
+        """Shows the newly updated time from time of instance creation"""
         self.updated_at = datetime.now()
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
-        """ Returns a Dictionary """
-
-        new_dict = self.__dict__.copy()
-        new_dict['created_at'] = self.created_at.isoformat()
-        new_dict['updated_at'] = self.updated_at.isoformat()
-        new_dict['__class__'] = self.__class__.__name__
-        return(new_dict)
+        """Returns a dictionay containing all keys/values"""
+        my_dict = {}
+        for key, item in self.__dict__.items():
+            if key in ['created_at', 'updated_at']:
+                my_dict[key] = item.isoformat()
+            else:
+                my_dict[key] = item
+        my_dict['__class__'] = self.__class__.__name__
+        return my_dict
