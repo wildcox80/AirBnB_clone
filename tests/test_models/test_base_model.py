4d145b5610081fb437c7f20ec147c8958d646a11
Wilder Rincon <1588@holbertonschool.com>
2020-06-30 20:31:28 -0500

    Adding changes file test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,149 +1,102 @@
 #!/usr/bin/python3
 """
-   unittest suite for model/base_model.py
+Unittest for base_model
 """
+
 import unittest
-import sys
 from models.base_model import BaseModel
-from datetime import datetime
-from io import StringIO
+import pep8
 
 
 class TestBaseModel(unittest.TestCase):
-    """class TestBaseClass"""
+    """
+    Test class BaseModel
+    """
 
-    def test_id(self):
-        """id attr test"""
-        model = BaseModel()
-        self.assertEqual(str, type(model.id))
+    def test_docstring(self):
+        """Checks if the docstring exists"""
+        self.assertTrue(len(BaseModel.__doc__) > 1)
+        self.assertTrue(len(BaseModel.__init__.__doc__) > 1)
+        self.assertTrue(len(BaseModel.__str__.__doc__) > 1)
+        self.assertTrue(len(BaseModel.save.__doc__) > 1)
+        self.assertTrue(len(BaseModel.to_dict.__doc__) > 1)
 
-    def test_created_at(self):
-        """created_at attr test"""
-        model = BaseModel()
-        self.assertEqual(datetime, type(model.created_at))
+    def test_pep8(self):
+        """Pep8 Test"""
+        style = pep8.StyleGuide(quiet=True)
+        result = style.check_files(['models/base_model.py',
+                                    'tests/test_models/test_base_model.py'])
+        self.assertEqual(result.total_errors, 0, "fix pep8")
 
-    def test_updated_at(self):
-        """created_at attr test"""
-        model = BaseModel()
-        self.assertEqual(datetime, type(model.updated_at))
+    def setUp(self):
+        """
+        Setups test
+        """
+        pass
 
-    def test_created_and_updated_at_init(self):
-        """created_at and updated_at attrs initialized
-        to current datetime test"""
-        model = BaseModel()
-        self.assertTrue(model.created_at, model.updated_at)
+    def test_init_arg(self):
+        """Pass an arg into the instance"""
+        b1 = BaseModel(12)
+        self.assertEqual(type(b1).__name__, "BaseModel")
+        self.assertFalse(hasattr(b1, "12"))
 
-    def test_save_updated_at(self):
-        """save method changing updated_at attr to current datetime test"""
-        model = BaseModel()
-        prev_updated_at = model.updated_at
-        model.save()
-        self.assertNotEqual(prev_updated_at, model.updated_at)
+    def test_init_kwarg(self):
+        """Pass kwargs into the instance"""
+        b1 = BaseModel(name="Red")
+        self.assertEqual(type(b1).__name__, "BaseModel")
+        self.assertTrue(hasattr(b1, "name"))
+        self.assertTrue(hasattr(b1, "__class__"))
+        self.assertFalse(hasattr(b1, "id"))
+        self.assertFalse(hasattr(b1, "created_at"))
+        self.assertFalse(hasattr(b1, "updated_at"))
 
-    def test_save_created_at(self):
-        """save method changing updated_at attr to current datetime test
-        but leaves created_at unchanged"""
-        model = BaseModel()
-        prev_created_at = model.created_at
-        model.save()
-        self.assertEqual(prev_created_at, model.created_at)
+    def test_str_method(self):
+        """Tests to see if the method is printing accurately"""
+        b1 = BaseModel()
+        b1printed = b1.__str__()
+        self.assertEqual(b1printed,
+                         "[BaseModel] ({}) {}".format(b1.id, b1.__dict__))
 
-    def test_to_dict_type(self):
-        """test to_dict returns dictionary"""
-        m = BaseModel()
-        model_dictionary = m.to_dict()
-        self.assertEqual(dict, type(model_dictionary))
+    def test_before_to_dict(self):
+        """Tests the instance before using the todict conversion"""
+        b1 = BaseModel()
+        b1_dict = b1.__dict__
+        self.assertEqual(type(b1).__name__, "BaseModel")
+        self.assertTrue(hasattr(b1, '__class__'))
+        self.assertEqual(str(b1.__class__),
+                         "<class 'models.base_model.BaseModel'>")
+        self.assertTrue(type(b1_dict['created_at']), 'datetime.datetime')
+        self.assertTrue(type(b1_dict['updated_at']), 'datetime.datetime')
+        self.assertTrue(type(b1_dict['id']), 'str')
 
-    def test_to_dict_class(self):
-        """test to_dict adds '__class__' to dictionary"""
-        m = BaseModel()
-        model_dictionary = m.to_dict()
-        self.assertIn('__class__', model_dictionary)
+    def test_after_to_dict(self):
+        """Tests instances after using to_dict conversion"""
+        my_model = BaseModel()
+        new_model = BaseModel()
+        test_dict = my_model.to_dict()
+        self.assertIsInstance(my_model, BaseModel)
+        self.assertEqual(type(my_model).__name__, "BaseModel")
+        self.assertEqual(test_dict['__class__'], "BaseModel")
+        self.assertTrue(type(test_dict['__class__']), 'str')
+        self.assertTrue(type(test_dict['created_at']), 'str')
+        self.assertTrue(type(test_dict['updated_at']), 'str')
+        self.assertTrue(type(test_dict['id']), 'str')
+        self.assertNotEqual(my_model.id, new_model.id)
 
-    def test_to_dict_id(self):
-        """test 'id' is in dictionary returned by to_dict method"""
-        m = BaseModel()
-        model_dictionary = m.to_dict()
-        self.assertIn('id', model_dictionary)
+    def test_has_attribute(self):
+        """Tests if the instance of BaseModel have been correctly made"""
+        b1 = BaseModel()
+        self.assertTrue(hasattr(b1, "__init__"))
+        self.assertTrue(hasattr(b1, "created_at"))
+        self.assertTrue(hasattr(b1, "updated_at"))
+        self.assertTrue(hasattr(b1, "id"))
 
-    def test_to_dict_id_str(self):
-        """test type of 'id' value is a str"""
-        m = BaseModel()
-        model_dictionary = m.to_dict()
-        self.assertEqual(str, type(model_dictionary['id']))
-
-    def test_to_dict_updated_at(self):
-        """test 'updated_at' is in dictionary returned by to_dict method"""
-        m = BaseModel()
-        model_dictionary = m.to_dict()
-        self.assertIn('updated_at', model_dictionary)
-
-    def test_to_dict_updated_at_str(self):
-        """test type of 'updated_at' value is a str"""
-        m = BaseModel()
-        model_dictionary = m.to_dict()
-        self.assertEqual(str, type(model_dictionary['updated_at']))
-
-    def test_to_dict_created_at(self):
-        """test 'created_at' is in dictionary returned by to_dict method"""
-        m = BaseModel()
-        model_dictionary = m.to_dict()
-        self.assertIn('created_at', model_dictionary)
-
-    def test_to_dict_created_at_str(self):
-        """test type of 'created_at' value is a str"""
-        m = BaseModel()
-        model_dictionary = m.to_dict()
-        self.assertEqual(str, type(model_dictionary['created_at']))
-
-    def test_dict_to_instance(self):
-        """test dict -> instance"""
-        m = BaseModel()
-        model_dictionary = m.to_dict()
-        m2 = BaseModel(**model_dictionary)
-        self.assertEqual(type(m), type(m2))
-
-    def test_dict_to_id_attr(self):
-        """test dict -> instance's id attr"""
-        m = BaseModel()
-        model_dictionary = m.to_dict()
-        m2 = BaseModel(**model_dictionary)
-        self.assertEqual(m.id, m2.id)
-
-    def test_dict_to_id_attr_type(self):
-        """test dict -> instance's id attr"""
-        m = BaseModel()
-        model_dictionary = m.to_dict()
-        m2 = BaseModel(**model_dictionary)
-        self.assertEqual(str, type(m2.id))
-
-    def test_dict_to_updated_at_attr(self):
-        """test dict -> instance's updated_at attr"""
-        m = BaseModel()
-        model_dictionary = m.to_dict()
-        m2 = BaseModel(**model_dictionary)
-        self.assertEqual(m.updated_at, m2.updated_at)
-
-    def test_dict_to_updated_at_attr_type(self):
-        """test dict -> instance's updated_at attr type"""
-        m = BaseModel()
-        model_dictionary = m.to_dict()
-        m2 = BaseModel(**model_dictionary)
-        self.assertEqual(type(datetime.now()), type(m2.updated_at))
-
-    def test_dict_to_created_at_attr(self):
-        """test dict -> instance's created_at attr"""
-        m = BaseModel()
-        model_dictionary = m.to_dict()
-        m2 = BaseModel(**model_dictionary)
-        self.assertEqual(m.created_at, m2.created_at)
-
-    def test_dict_to_created_at_attr_type(self):
-        """test dict -> instance's created_at attr type"""
-        m = BaseModel()
-        model_dictionary = m.to_dict()
-        m2 = BaseModel(**model_dictionary)
-        self.assertEqual(type(datetime.now()), type(m2.created_at))
+    def test_save(self):
+        """Tests to see if the save function works"""
+        b1 = BaseModel()
+        b1.save()
+        b_dict = b1.to_dict()
+        self.assertNotEqual(b_dict['created_at'], b_dict['updated_at'])
 
 
 if __name__ == '__main__':
