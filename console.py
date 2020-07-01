43e1cb74c83e28a03f340df03ebb4f45050ae4b8
Wilder Rincon <1588@holbertonschool.com>
2020-06-30 20:39:50 -0500

    Adding changes file console.py

--- console.py
+++ console.py
@@ -15,12 +15,6 @@ class HBNBCommand(cmd.Cmd):
         """EOF command to exit the console\n"""
         return True
 
-    def do_help(self, arg):
-        """Help command to get info on specified command
-        If no parameter given, list all commands
-        """
-        cmd.Cmd.do_help(self, arg)
-
     def emptyline(self):
         """ Empty Line  """
         pass
