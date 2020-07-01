21d9269278af174eb688aba3effa5561fde668c3
“josebant29” <josebant29@gmail.com>
2020-07-01 01:26:09 -0500

    amenity and place

---
+++ models/place.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Place Model"""
+from models.base_model import BaseModel
+
+
+class Place(BaseModel):
+    """strcuture of the db"""
+    city_id = ""  # City.id format
+    user_id = ""  # User.id format
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = ""  # Amenity.id format
