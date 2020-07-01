19012d452909c2f4141a72b91c8665f0b15fbd10
Wilder Rincon <1588@holbertonschool.com>
2020-07-01 17:13:42 -0500

    Adding changes file test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,93 +1,126 @@
 #!/usr/bin/python3
-"""
-Unittest for base_model
-"""
-
+"""Module for test BaseModel class"""
 import unittest
-from models.base_model import BaseModel
+import json
 import pep8
+import datetime
+from time import sleep
+
+from models.base_model import BaseModel
 
 
 class TestBaseModel(unittest.TestCase):
-    """
-    Test class BaseModel
-    """
-    def test_docstring(self):
-        """Checks if the docstring exists"""
-        self.assertTrue(len(BaseModel.__doc__) > 1)
-        self.assertTrue(len(BaseModel.__init__.__doc__) > 1)
-        self.assertTrue(len(BaseModel.__str__.__doc__) > 1)
-        self.assertTrue(len(BaseModel.save.__doc__) > 1)
-        self.assertTrue(len(BaseModel.to_dict.__doc__) > 1)
+    """Test for BaseModel class"""
 
-    def test_pep8(self):
-        """Pep8 Test"""
-        style = pep8.StyleGuide(quiet=True)
-        result = style.check_files(['models/base_model.py'])
-        self.assertEqual(result.total_errors, 0, "fix pep8")
+    def test_doc_module(self):
+        """Module documentation"""
+        doc = BaseModel.__doc__
+        self.assertGreater(len(doc), 1)
 
-    def setUp(self):
-        """
-        Setups test
-        """
-        pass
+    def test_pep8_conformance_base_model(self):
+        """Test that models/base_model.py conforms to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/base_model.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
-    def test_init_arg(self):
-        """Pass an arg into the instance"""
-        b1 = BaseModel(12)
-        self.assertEqual(type(b1).__name__, "BaseModel")
-        self.assertFalse(hasattr(b1, "12"))
+    def test_pep8_conformance_test_base_model(self):
+        """Test that tests/test_models/test_base_model.py conforms to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        res = pep8style.check_files(['tests/test_models/test_base_model.py'])
+        self.assertEqual(res.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
-    def test_init_kwarg(self):
-        """Pass kwargs into the instance"""
-        b1 = BaseModel(name="Red")
-        self.assertEqual(type(b1).__name__, "BaseModel")
-        self.assertTrue(hasattr(b1, "name"))
-        self.assertTrue(hasattr(b1, "__class__"))
-        self.assertFalse(hasattr(b1, "id"))
-        self.assertFalse(hasattr(b1, "created_at"))
-        self.assertFalse(hasattr(b1, "updated_at"))
+    def test_doc_constructor(self):
+        """Constructor documentation"""
+        doc = BaseModel.__init__.__doc__
+        self.assertGreater(len(doc), 1)
 
-    def test_str_method(self):
-        """Tests to see if the method is printing accurately"""
-        b1 = BaseModel()
-        b1printed = b1.__str__()
-        self.assertEqual(b1printed,
-                         "[BaseModel] ({}) {}".format(b1.id, b1.__dict__))
-
-    def test_before_to_dict(self):
-        """Tests the instance before using the todict conversion"""
-        b1 = BaseModel()
-        b1_dict = b1.__dict__
-        self.assertEqual(type(b1).__name__, "BaseModel")
-        self.assertTrue(hasattr(b1, '__class__'))
-        self.assertEqual(str(b1.__class__),
-                         "<class 'models.base_model.BaseModel'>")
-        self.assertTrue(type(b1_dict['created_at']), 'datetime.datetime')
-        self.assertTrue(type(b1_dict['updated_at']), 'datetime.datetime')
-        self.assertTrue(type(b1_dict['id']), 'str')
-
-    def test_after_to_dict(self):
-        """Tests instances after using to_dict conversion"""
+    def test_first_task(self):
+        """Test creation of class and to_dict"""
         my_model = BaseModel()
-        new_model = BaseModel()
-        test_dict = my_model.to_dict()
-        self.assertIsInstance(my_model, BaseModel)
-        self.assertEqual(type(my_model).__name__, "BaseModel")
-        self.assertEqual(test_dict['__class__'], "BaseModel")
-        self.assertTrue(type(test_dict['__class__']), 'str')
-        self.assertTrue(type(test_dict['created_at']), 'str')
-        self.assertTrue(type(test_dict['updated_at']), 'str')
-        self.assertTrue(type(test_dict['id']), 'str')
-        self.assertNotEqual(my_model.id, new_model.id)
+        self.assertIs(type(my_model), BaseModel)
+        my_model.name = "Holberton"
+        my_model.my_number = 89
+        self.assertEqual(my_model.name, "Holberton")
+        self.assertEqual(my_model.my_number, 89)
+        model_types_json = {
+            "my_number": int,
+            "name": str,
+            "__class__": str,
+            "updated_at": str,
+            "id": str,
+            "created_at": str
+        }
+        my_model_json = my_model.to_dict()
+        for key, value in model_types_json.items():
+            with self.subTest(key=key, value=value):
+                self.assertIn(key, my_model_json)
+                self.assertIs(type(my_model_json[key]), value)
 
-    def test_has_attribute(self):
-        """Tests if the instance of BaseModel have been correctly made"""
-        b1 = BaseModel()
-        self.assertTrue(hasattr(b1, "__init__"))
-        self.assertTrue(hasattr(b1, "created_at"))
-        self.assertTrue(hasattr(b1, "updated_at"))
-        self.assertTrue(hasattr(b1, "id"))
+    def test_base_types(self):
+        """Testing dict model"""
+        second_model = BaseModel()
+        self.assertIs(type(second_model), BaseModel)
+        second_model.name = "Andres"
+        second_model.my_number = 80
+        self.assertEqual(second_model.name, "Andres")
+        self.assertEqual(second_model.my_number, 80)
+        model_types = {
+            "my_number": int,
+            "name": str,
+            "updated_at": datetime.datetime,
+            "id": str,
+            "created_at": datetime.datetime
+            }
+        for key, value in model_types.items():
+            with self.subTest(key=key, value=value):
+                self.assertIn(key, second_model.__dict__)
+                self.assertIs(type(second_model.__dict__[key]), value)
+
+    def test_uuid(self):
+        """testing different uuid"""
+        model = BaseModel()
+        model_2 = BaseModel()
+        self.assertNotEqual(model.id, model_2.id)
+
+    def test_datetime_model(self):
+        """testing datetime base model"""
+        model_3 = BaseModel()
+        model_4 = BaseModel()
+        self.assertNotEqual(model_3.created_at, model_3.updated_at)
+        self.assertNotEqual(model_3.created_at, model_4.created_at)
+
+    def test_string_representation(self):
+        """Test the magic method str"""
+        my_model = BaseModel()
+        my_model.name = "Holberton"
+        my_model.my_number = 89
+        id_model = my_model.id
+
+        expected = '[BaseModel] ({}) {}'\
+                   .format(id_model, my_model.__dict__)
+        self.assertEqual(str(my_model), expected)
+
+    def test_constructor_kwargs(self):
+        """Test constructor that has kwargs as attributes values"""
+        obj = BaseModel()
+        obj.name = "Holberton"
+        obj.my_number = 89
+        json_attributes = obj.to_dict()
+
+        obj2 = BaseModel(**json_attributes)
+
+        self.assertIsInstance(obj2, BaseModel)
+        self.assertIsInstance(json_attributes, dict)
+        self.assertIsNot(obj, obj2)
+
+    def test_file_save(self):
+        """Test that info is saved to file"""
+        b3 = BaseModel()
+        b3.save()
+        with open("file.json", 'r') as f:
+            self.assertIn(b3.id, f.read())
 
 
 if __name__ == '__main__':
