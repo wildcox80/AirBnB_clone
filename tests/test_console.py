20d20f4adfd561826463a7ac1599894292315493
Wilder Rincon <1588@holbertonschool.com>
2020-06-30 20:25:44 -0500

    Adding file test_console.py

---
+++ tests/test_console.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Unittest for Console"""
+
+from console import HBNBCommand
+import sys
+import unittest
+from unittest.mock import create_autospec
+
+
+class TestConsole(unittest.TestCase):
+    """Console Unittest"""
+
+    def setUp(self):
+        """Sets up STDIN and STDOUT"""
+        self.mock_stdin = create_autospec(sys.stdin)
+        self.mock_stdout = create_autospec(sys.stdout)
+
+    def create(self, server=None):
+        """Creates HBNBCommand"""
+        return HBNBCommand(stdin=self.mock_stdin, stdout=self.mock_stdout)
+
+    def test_quit(self):
+        """Tests the quit command"""
+        xit = self.create()
+        self.assertTrue(xit.onecmd("quit"))
+
+
+if __name__ == '__main__':
+    unittest.main()
