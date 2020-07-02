06d68084739403bdd0f0c652b3371face7a1f430
“josebant29” <josebant29@gmail.com>
2020-07-01 23:06:59 -0500

    pep8 style apllied all

--- models/user.py
+++ models/user.py
@@ -10,7 +10,6 @@ class User(BaseModel):
     first_name = ""
     last_name = ""
 
-
     def __init__(self, *args, **kwargs):
         """User constructor"""
         super().__init__(*args, **kwargs)
