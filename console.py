3f4445fe32abb41b0078fc150f8c66f979fb0053
Wilder Rincon <1588@holbertonschool.com>
2020-07-01 19:01:13 -0500

    Adding changes file console.py

--- console.py
+++ console.py
@@ -21,6 +21,124 @@ class HBNBCommand(cmd.Cmd):
         """ Empty Line  """
         pass
 
+    def do_create(self, line):
+        """Creates a new instance of BaseModel,
+        saves it (to the JSON file) and prints the id.
+        Ex: $ create BaseModel
+        """
+        args = line.split()
+
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        else:
+            new_inst = eval(args[0] + '()')
+            models.storage.save()
+            print(new_inst.id)
+
+    def do_show(self, line):
+        """Prints the string representation of an instance
+        based on the class name and id.
+        Ex: $ show BaseModel 1234-1234-1234
+        """
+        args = line.split()
+        objects = models.storage.all()
+
+        if len(args) == 0:
+            print('** class name missing **')
+        elif args[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print('** instance id missing **')
+        else:
+            key_find = args[0] + '.' + args[1]
+            if key_find in objects.keys():
+                print(objects[key_find])
+            else:
+                print('** no instance found **')
+
+    def do_destroy(self, line):
+        """Deletes an instance based on the class name and id
+        (save the change into the JSON file).
+        Ex: $ destroy BaseModel 1234-1234-1234
+        """
+        args = line.split()
+        objects = models.storage.all()
+
+        if len(args) == 0:
+            print('** class name missing **')
+        elif args[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print('** instance id missing **')
+        else:
+            key_find = args[0] + '.' + args[1]
+            if key_find in objects.keys():
+                objects.pop(key_find, None)
+                models.storage.save()
+            else:
+                print('** no instance found **')
+
+    def do_all(self, line):
+        """Prints all string representation of all instances
+        based or not on the class name.
+        Ex: $ all BaseModel
+        """
+        args = line.split()
+        objects = models.storage.all()
+        new_list = []
+
+        if len(args) == 0:
+            for obj in objects.values():
+                new_list.append(obj.__str__())
+            print(new_list)
+
+        elif args[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+
+        else:
+            for obj in objects.values():
+                if obj.__class__.__name__ == args[0]:
+                    new_list.append(obj.__str__())
+            print(new_list)
+
+    def do_update(self, line):
+        """
+        Updates an instance based on the class name
+        and id by adding or updating attribute (save
+        the change into the JSON file).
+        Ex: $ update BaseModel <valid id> attrib value
+        """
+        args = line.split()
+        objects = models.storage.all()
+
+        if len(args) == 0:
+            print("** class name missing **")
+
+        elif args[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+
+        elif len(args) == 1:
+            print("** instance id missing **")
+
+        elif len(args) == 2:
+            print("** attribute name missing **")
+
+        elif len(args) == 3:
+            print("** value missing **")
+
+        else:
+            key_find = args[0] + '.' + args[1]
+            obj = objects.get(key_find, None)
+
+            if not obj:
+                print("** no instance found **")
+                return
+
+            setattr(obj, args[2], args[3].lstrip('"').rstrip('"'))
+            models.storage.save()
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
